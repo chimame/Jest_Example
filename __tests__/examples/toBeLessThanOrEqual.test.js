af1a99d18f74e27ef6794ca08bf76e6ebfacffa8
chimame <rito.tamata@gmail.com>
2016-10-13 10:25:28 +0900

    Add toBeLessThanOrEqual example

---
+++ __tests__/examples/toBeLessThanOrEqual.test.js
@@ -0,0 +1,9 @@
+describe('toBeLessThanOrEqual example', () => {
+  test('less than 10 or equal', () => {
+    expect(9).toBeLessThanOrEqual(10)
+  })
+
+  test('less than 10 or equal', () => {
+    expect(10).toBeLessThanOrEqual(10)
+  })
+})
